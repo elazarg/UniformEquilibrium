a17a359933392bd789ce84d05d5fb9debb09889e
Elazar Gershuni <elazarg@gmail.com>
2026-08-21 08:36:59 +0300

    Generalize play starts and Q-boundary directions

--- UniformEquilibrium/Quitting/AbsorptionPath/All.lean
+++ UniformEquilibrium/Quitting/AbsorptionPath/All.lean
@@ -14,6 +14,7 @@ import UniformEquilibrium.Quitting.AbsorptionPath.CollisionConcentration
 import UniformEquilibrium.Quitting.AbsorptionPath.NormalizedFiniteWindowOccupation
 import UniformEquilibrium.Quitting.AbsorptionPath.FiniteWindowRefusalReweighting
 import UniformEquilibrium.Quitting.AbsorptionPath.PrincipalQDirection
+import UniformEquilibrium.Quitting.AbsorptionPath.PrincipalQBoundaryDirection
 
 /-!
 # Marked absorption-path semantic layer
